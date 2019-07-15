8c499bb4008eff5703d51122892bc6bd67069b8e
Gideon Ibemere Jr <gideon.ibemere.jr@gmail.com>
2019-07-15 10:52:39 -0500

    Add cubic-bezier on hover. Archive Page complete

--- src/components/ArchiveLink/index.jsx
+++ src/components/ArchiveLink/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+
 const ArchiveLink = () => {
   return (
     <div>
-      <Link className="button bg-transparent contain button-reset mixcloud-link db bg-center pointer center"></Link>
+      <figure className="button bg-transparent contain button-reset mixcloud-link db bg-center pointer center" />
     </div>
   )
 }
--- src/pages/Archive/index.jsx
+++ src/pages/Archive/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import PlayMix from '../../components/PlayMix'
-import PlayButton from '../../components/PlayButton'
+
 import ArchiveLink from '../../components/ArchiveLink'
 import util from '../../utils/utils'
 const Archive = ({ mixes, ...props }) => {
+  console.log(mixes)
   return (
     // <ul className="list pl0 archive mv0 pad-bottom">
     //   {mixes.map((mix, idx) => (
@@ -24,18 +24,17 @@ const Archive = ({ mixes, ...props }) => {
       <tbody className="list pl0 mr5 archive mv0 pad-bottom">
         {mixes.map((mix, idx) => (
           //<PlayMix {...props}>
-            <tr
-              key={idx}
-              className="pv3 ml4 mr4 bb b--light-gray flex justify-between items-center"
-            >
-              <td>
-                <h1 className="f6 mv0 black ttu biryani pr2">{mix.name}</h1>
-              </td>
-              <td>
-          <ArchiveLink/>
-               
-              </td>
-            </tr>
+          <tr
+            key={idx}
+            className="pv3 ml4 mr4 bb b--light-gray flex justify-between items-center"
+          >
+            <td>
+              <h1 className="f6 mv0 black ttu biryani pr2">{mix.name}</h1>
+            </td>
+            <td>
+              <ArchiveLink {...mix} />
+            </td>
+          </tr>
           // </PlayMix>
         ))}
       </tbody>
